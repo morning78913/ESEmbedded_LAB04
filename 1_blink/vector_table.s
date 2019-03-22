b768f979ee7b2450f888da797a8c2ba53d3a9410
morning78913 <morning78913@gmail.com>
2019-03-22 22:23:46 +0800

    update LAB04

--- 1_blink/vector_table.s
+++ 1_blink/vector_table.s
@@ -1,4 +1,14 @@
 .section .vector_table
 
 .word 0x20000100
+.word main
+
+
+
+
+/**************************************
+.section .vector_table
+
+.word 0x20000100
 .word blink
+**************************************/
